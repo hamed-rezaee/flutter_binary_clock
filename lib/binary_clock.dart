9c8aaf45f72e932cf65b0f5e7fc677ff74e02680
Hamed Rezaee <57184669+hamed-deriv@users.noreply.github.com>
2023-09-17 12:32:33 +0800

    add single row clock

--- lib/binary_clock.dart
+++ lib/binary_clock.dart
@@ -54,11 +54,101 @@ class BinaryClock {
     }
   }
 
+  void drawBinaryDigitSingleRow(
+    Canvas canvas,
+    Size size,
+    int hour,
+    int minute,
+    int second,
+  ) {
+    const Radius borderRadius = Radius.circular(4);
+
+    final List<bool> hourBinary = _getBinary(hour);
+    final List<bool> minuteBinary = _getBinary(minute);
+    final List<bool> secondBinary = _getBinary(second);
+    final List<bool> millisecondBinary = _getBinary(millisecond);
+
+    final Paint paint = Paint()
+      ..strokeWidth = 2
+      ..maskFilter = const MaskFilter.blur(BlurStyle.solid, 16);
+
+    for (int i = 0; i < hourBinary.length; i++) {
+      final double x = size.width / 2 + (i - 1) * size.width / 4;
+      final double y = size.height / 2;
+      final Offset center = Offset(x, y);
+
+      final double hourSize = size.width / 5;
+      final double minuteSize = size.width / 7;
+      final double secondSize = size.width / 12;
+      final double millisecondSize = size.width / 25;
+
+      canvas
+        ..drawRRect(
+          RRect.fromRectAndRadius(
+            Rect.fromCenter(center: center, width: hourSize, height: hourSize),
+            borderRadius,
+          ),
+          paint
+            ..color = Colors.green.withOpacity(0.3)
+            ..style = PaintingStyle.stroke,
+        )
+        ..drawRRect(
+          RRect.fromRectAndRadius(
+            Rect.fromCenter(center: center, width: hourSize, height: hourSize),
+            borderRadius,
+          ),
+          paint
+            ..color = hourBinary[i] ? Colors.green[900]! : Colors.transparent
+            ..style = PaintingStyle.fill,
+        )
+        ..drawRRect(
+          RRect.fromRectAndRadius(
+            Rect.fromCenter(
+              center: center,
+              width: minuteSize,
+              height: minuteSize,
+            ),
+            borderRadius,
+          ),
+          paint
+            ..color = minuteBinary[i] ? Colors.green[600]! : Colors.transparent
+            ..style = PaintingStyle.fill,
+        )
+        ..drawRRect(
+          RRect.fromRectAndRadius(
+            Rect.fromCenter(
+              center: center,
+              width: secondSize,
+              height: secondSize,
+            ),
+            borderRadius,
+          ),
+          paint
+            ..color = secondBinary[i] ? Colors.green[300]! : Colors.transparent
+            ..style = PaintingStyle.fill,
+        )
+        ..drawRRect(
+          RRect.fromRectAndRadius(
+            Rect.fromCenter(
+              center: center,
+              width: millisecondSize,
+              height: millisecondSize,
+            ),
+            borderRadius,
+          ),
+          paint
+            ..color =
+                millisecondBinary[i] ? Colors.green[100]! : Colors.transparent
+            ..style = PaintingStyle.fill,
+        );
+    }
+  }
+
   void drawText(
     Canvas canvas,
     Size size,
     String text, [
-    Color color = Colors.yellow,
+    Color color = Colors.green,
   ]) {
     final TextPainter painter = TextPainter(
       textDirection: TextDirection.ltr,
@@ -69,7 +159,7 @@ class BinaryClock {
           fontSize: size.width / 8,
           fontWeight: FontWeight.bold,
           fontFamily: GoogleFonts.oxanium().fontFamily,
-          shadows: <Shadow>[Shadow(color: color, blurRadius: 16)],
+          shadows: <Shadow>[Shadow(color: color, blurRadius: 8)],
         ),
       ),
     )..layout(minWidth: size.width);
@@ -86,7 +176,7 @@ class BinaryClock {
   List<bool> _getBinary(int value) {
     final List<bool> binary = <bool>[];
 
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 6; i++) {
       binary.add(value % 2 == 1);
       value = value ~/ 2;
     }
--- lib/binary_clock_painter.dart
+++ lib/binary_clock_painter.dart
@@ -3,6 +3,9 @@ import 'package:intl/intl.dart' show NumberFormat;
 
 import 'package:flutter_binary_clock/binary_clock.dart';
 
+final NumberFormat numberFormat2Digits = NumberFormat('00');
+final NumberFormat numberFormat3Digits = NumberFormat('000');
+
 class BinaryClockPainter extends CustomPainter {
   BinaryClockPainter(this.binaryClock);
 
@@ -10,13 +13,35 @@ class BinaryClockPainter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
-    final NumberFormat numberFormat = NumberFormat('00');
+    _drawSingleRowClock(canvas, size);
 
+    canvas.translate(0, 128);
+
+    _drawClock(canvas, size);
+  }
+
+  void _drawSingleRowClock(Canvas canvas, Size size) {
     binaryClock
       ..drawText(
         canvas,
         size,
-        'Hour ${numberFormat.format(binaryClock.hour)}',
+        '${numberFormat2Digits.format(binaryClock.hour)} : ${numberFormat2Digits.format(binaryClock.minute)} : ${numberFormat2Digits.format(binaryClock.second)} : ${numberFormat3Digits.format(binaryClock.millisecond)}',
+      )
+      ..drawBinaryDigitSingleRow(
+        canvas,
+        size,
+        binaryClock.hour,
+        binaryClock.minute,
+        binaryClock.second,
+      );
+  }
+
+  void _drawClock(Canvas canvas, Size size) {
+    binaryClock
+      ..drawText(
+        canvas,
+        size,
+        'Hour ${numberFormat2Digits.format(binaryClock.hour)}',
       )
       ..drawBinaryDigit(canvas, size, binaryClock.hour);
 
@@ -25,7 +50,7 @@ class BinaryClockPainter extends CustomPainter {
       ..drawText(
         canvas,
         size,
-        'Minute ${numberFormat.format(binaryClock.minute)}',
+        'Minute ${numberFormat2Digits.format(binaryClock.minute)}',
       )
       ..drawBinaryDigit(canvas, size, binaryClock.minute);
 
@@ -34,7 +59,7 @@ class BinaryClockPainter extends CustomPainter {
       ..drawText(
         canvas,
         size,
-        'Second ${numberFormat.format(binaryClock.second)}',
+        'Second ${numberFormat2Digits.format(binaryClock.second)}',
       )
       ..drawBinaryDigit(canvas, size, binaryClock.second);
 
@@ -43,7 +68,7 @@ class BinaryClockPainter extends CustomPainter {
       ..drawText(
         canvas,
         size,
-        'Millisecond ${numberFormat.format(binaryClock.millisecond)}',
+        'Millisecond ${numberFormat3Digits.format(binaryClock.millisecond)}',
       )
       ..drawBinaryDigit(canvas, size, binaryClock.millisecond);
   }
